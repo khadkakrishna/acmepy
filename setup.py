70b6bea4d30f4e2af766e40998ae74537f130df9
khadkakrishna <krishnakhadka2802@gmail.com>
2021-06-13 20:47:37 +0530

    Added v_0.1 to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
     name='acmepy',
-    packages = ['acmepy'],
     version='0.1',
     license='Apache 2.0',
     description = 'Acme client to generate SSL Certificates for the websites',
@@ -15,18 +14,18 @@ setup(
         'acme'
     ],
     classifiers = [
-    'Development Status :: 1 - Production/Stable',
-    'Intended Audience :: System Administrators and Developers',
-    'License :: OSI Approved :: Apache 2.0 License',
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',    
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10'
     ]
 )
